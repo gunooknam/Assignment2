d4166dfcbb9c97825848114c8ecaea5622254cc2
JooHyungByun <pa_be@naver.com>
2018-04-25 00:33:35 -0700

    add file time/date compare function

--- main.c
+++ main.c
@@ -34,33 +34,33 @@ int main(void)
 
 //파일 존재하지 않을 경우 예외처리를 출력
 void errorcheck(){
-  switch(errno){
-  case 2:
-	 perror("text1 ");
-	 exit(0);
-  case 4: 
-	 perror("text2 ");
-	 exit(0);
-  case 8: 
-	 perror("text1 and text2 ");
-	 exit(0);
-  }
+	switch(errno){
+		case 2:
+			perror("text1 ");
+			exit(0);
+		case 4: 
+			perror("text2 ");
+			exit(0);
+		case 8: 
+			perror("text1 and text2 ");
+			exit(0);
+	}
 }
 
 //파일 1의 정보를 가져오는 함수 작성
 void filestat1(void)
 {
-     if(stat("text1", &stat1)){
-         errno=errno<<1;
-     } 
+	if(stat("text1", &stat1)){
+		errno=errno<<1;
+	} 
 }
 
 //파일 2의 정보를 가져오는 함수 작성
 void filestat2(void)
 {
-     if(stat("text2", &stat2)){
-         errno=errno<<2;
-     }
+	if(stat("text2", &stat2)){
+		errno=errno<<2;
+	}
 }
 
 //파일 1의 시간 정보를 가져오는 함수 작성
@@ -76,33 +76,49 @@ void filetime2(void)
 //두 개의 파일 크기를 비교하는 함수 작성
 void sizecmp(void)
 {
-   printf("size compare\n");
-   if ( stat1.st_size > stat2.st_size ) 
-        printf("text1 is bigger\n\n");
-   else if (stat1.st_size < stat2.st_size ) 
-	printf("text2 is bigger\n\n");
-   else 
-	printf("sizes are equal\n\n");
+	printf("size compare\n");
+	if ( stat1.st_size > stat2.st_size ) 
+		printf("text1 is bigger\n\n");
+	else if (stat1.st_size < stat2.st_size ) 
+		printf("text2 is bigger\n\n");
+	else 
+		printf("sizes are equal\n\n");
 }
 
 //두 개의 파일 블락 수를 비교하는 함수 작성
 void blockcmp(void)
 {
-   printf("block compare\n");
-   if (stat1.st_blocks > stat2.st_blocks) 
-        printf("text1 is bigger\n\n");
-   else if(stat1.st_blocks < stat2.st_blocks) 
-	printf("text2 is bigger\n\n");
-   else
-	printf("sizes are equal\n\n");
+	printf("block compare\n");
+	if (stat1.st_blocks > stat2.st_blocks) 
+		printf("text1 is bigger\n\n");
+	else if(stat1.st_blocks < stat2.st_blocks) 
+		printf("text2 is bigger\n\n");
+	else
+		printf("sizes are equal\n\n");
 }
 
 //두 개의 파일 수정 날짜를 비교하는 함수 작성
 void datecmp(void)
 {
+	printf("date compare\n");  
+	if(time1.tm_mon+1 > time2.tm_mon+1)
+		printf("text2 is early\n\n");
+	else if(time1.tm_mon+1 < time2.tm_mon+1)
+		printf("text1 is early\n\n");
+	else {
+		(time1.tm_mday > time2.tm_mday)? printf("text2 is early\n\n"): ( (time1.tm_mday <time2.tm_mday)? printf("text1 is early\n\n"):printf("same date\n\n") );  
+	}
 }
 
 //두 개의 파일 수정 시간을 비교하는 함수 작성
 void timecmp(void)
 {
+	printf("time compare\n");
+	if(time1.tm_hour > time2.tm_hour)
+        	printf("text2 is early\n\n");
+  	else if((int)time1.tm_hour < (int)time2.tm_hour)
+        	printf("text1 is early\n\n");    
+	else {
+	(time1.tm_min > time2.tm_min)? printf("text2 is early\n\n"): ( (time1.tm_min <time2.tm_min)? printf("text1 is early\n\n"):printf("same time\n\n") );  
+}  
 }
